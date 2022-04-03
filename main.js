ba35c1dbf90220bdd962a27fb7fac6027903c869
Yale Work <yale.work@gmail.com>
2022-04-03 01:00:24 -0600

    more cleanup

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const interval = setInterval(() => {
-        const video = document.querySelector('video');
-        if (video) {
-            video.addEventListener('loadeddata', () => {
+const COMMERCIAL_BREAK_SECONDS = 90;
+
+const addKeyboardControls = (video) => {
+    const vidCtrl = (e) => {
+        const key = e.code;
+
+        if (key === 'ArrowLeft') {
+            const timeBack = e.shiftKey ? COMMERCIAL_BREAK_SECONDS : 5;
+            video.currentTime -= timeBack;
+            if (video.currentTime < 0) {
+                video.pause();
                 video.currentTime = 0;
-                video.muted = false;
-                clearInterval(interval);
-            });
+            }
+        } else if (key === 'ArrowRight') {
+            const timeForward = e.shiftKey ? COMMERCIAL_BREAK_SECONDS : 5;
+            video.currentTime += timeForward;
+            if (video.currentTime > video.duration) {
+                video.pause();
+                video.currentTime = 0;
+            }
         }
-    }, 500);
+    }
+
+    window.onkeydown = vidCtrl;
+};
+
+const configureVideo = (video) => {
+    video.addEventListener('loadeddata', () => {
+        video.currentTime = 0;
+        video.muted = false;
+        addKeyboardControls(video);
+    });
+}
+
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(mutation => {
+        if (mutation.type === 'attributes' &&
+                mutation.target.nodeName === 'VIDEO' &&
+                mutation.attributeName === 'src') {
+            configureVideo(mutation.target);
+        }
+    });
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
 });
